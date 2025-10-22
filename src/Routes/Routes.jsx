842397962a2fedbbb47469f3824aeb348a7b72e1
fardoush <fardoushjahanara@gmail.com>
2025-10-22 22:40:14 +0600

    project setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createBrowserRouter } from "react-router";
+import Home from '../pages/Home/Home';
+import Root from '../pages/Root/Root';
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children:[
+       {
+        index: true, Component: Home 
+       }
+
+    ]
+  },
+]);
