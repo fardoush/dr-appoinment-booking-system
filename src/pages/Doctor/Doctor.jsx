11b8352bc7425ae04807a89c364541cc088ea092
fardoush <fardoushjahanara@gmail.com>
2025-10-24 19:46:39 +0600

    add to see more btn and functionality

--- src/pages/Doctor/Doctor.jsx
+++ src/pages/Doctor/Doctor.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
+
 import { FaHandHoldingMedical } from "react-icons/fa";
 
 
 const Doctor = ({singleData}) => {
-    const {image,available,experience_years,name,education,registration_number,availability} = singleData;
+    
+    const {image,available,experience_years,name,education,registration_number,} = singleData;
+
+
+
     return (
-        <div className="card bg-white text-black w-96 shadow-sm">
+        <div className="card bg-white text-black w-full shadow-sm">
   <figure>
     <img
       src={image}
-      alt="mg" />
+      alt={name} />
   </figure>
   <div className="card-body">
     <div className="card-actions ">
-      <div className={`badge rounded-full ${ available === "Available" ? "text-green-600 bg-green-100 border-green-600" : "text-red-400 bg-red-100 border-red-400"}`}>{availability?.available ? "Available" : "Not Available"}
+      <div className={`badge rounded-full ${ available === "Available" ? "text-green-600 bg-green-100 border-green-600" : "text-red-400 bg-red-100 border-red-400"}`}>{available}
 
       </div>
       <div className="badge text-blue-600 bg-blue-100 border-blue-600 rounded-full">{experience_years}+ Years Experience</div>
@@ -23,11 +27,12 @@ const Doctor = ({singleData}) => {
       
     </h2>
     <p>{education}</p>
-    <h6 className="text-xl py-4 border-t-2 border-dashed border-gray-200"><FaHandHoldingMedical className='inline-block mr-2 ' /> Reg No: 
- {registration_number}</h6>
-    <button className='btn btn-full rounded-full bg-transparent  border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white shadow-none transition duration-300 ease-in-out
- '>View Details</button>
+    <h6 className="text-xl py-4 border-t-2 border-dashed border-gray-200"><FaHandHoldingMedical className='inline-block mr-2 text-blue-500' /> Reg No: {registration_number}</h6>
     
+
+         <button  className='btn btn-full rounded-full bg-transparent  border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white shadow-none transition duration-300 ease-in-out
+ '>View Details</button>
+ 
   </div>
 </div>
     );
--- src/pages/Doctors/Doctors.jsx
+++ src/pages/Doctors/Doctors.jsx
@@ -1,23 +1,43 @@
-import React, { Suspense } from 'react';
-import Doctor from '../Doctor/Doctor';
+import React, { Suspense } from "react";
+import Doctor from "../Doctor/Doctor";
+import { useState } from "react";
 
-const Doctors = ({data}) => {
+const Doctors = ({ data }) => {
+  const [visible, setVisible] = useState(6);
 
-    return (
-        <div  className='py-20 w-6xl mx-auto flex items-center  justify-center flex-col'>
-           <div className=" w-3xl flex justify-center items-center flex-col">
-             <h2 className="text-4xl text-center">Our Best Doctors</h2>
-            <p className="text-center text-lg">Our platform connects you with verified, experienced doctors across various specialties — all at your convenience. Whether it's a routine checkup or urgent consultation, book appointments in minutes and receive quality care you can trust.</p>
-           </div>
+      const handleLoadMore = () => {
+        // console.log(singleData.length);
+        setVisible(data.length);
 
-           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-{
-    data.map(singleData => <Doctor singleData={singleData} key={singleData.id}></Doctor>)
-}
-           </div>
-            <Suspense/>
-        </div>
-    );
+    }
+  return (
+    <div className="py-15 w-6xl mx-auto flex items-center  justify-center flex-col">
+      <div className=" w-3xl flex justify-center items-center flex-col pb-10">
+        <h2 className="text-4xl text-center pb-4">Our Best Doctors</h2>
+        <p className="text-center text-lg">
+          Our platform connects you with verified, experienced doctors across
+          various specialties — all at your convenience. Whether it's a routine
+          checkup or urgent consultation, book appointments in minutes and
+          receive quality care you can trust.
+        </p>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7">
+        {data.slice(0, visible).map((singleData) => (
+          <Doctor singleData={singleData} key={singleData.id} handleLoadMore={handleLoadMore}></Doctor>
+        ))}
+      </div>
+
+      {
+        visible < data.length && (
+                <button onClick={handleLoadMore} className="mt-8 btn rounded-full bg-blue-400 text-white border-none shadow-none ">Show More</button>
+
+        )
+      }
+
+      <Suspense />
+    </div>
+  );
 };
 
 export default Doctors;
